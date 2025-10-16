67d8673302cc68402ab613c7d51bc7575b49a9cd
Nnamani John <nnamanijohn123@gmail.com>
2025-10-16 01:10:21 +0100

    add components and update nuxt config for Tailwind CSS and shadcn-nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/test-utils']
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt'
+  ]
 })
